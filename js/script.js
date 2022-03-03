aabf504cd89a33c31e565593e8cb7c84a7baef5a
Ismail Hossain <ismail.9595@gmail.com>
2022-03-03 10:51:24 +0600

    Added a script file

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// __________________________________________________________ //
+// ____________________ Set current year ____________________ //
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// _____________________________________________________________________ //
+// ____________________ Make mobile navigation work ____________________ //
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// ____________________________________________________________________ //
+// ____________________ Smooth scrolling animation ____________________ //
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("header_navbar-link")) headerEl.classList.toggle("nav-open");
+  });
+});
+
+// ___________________________________________________________ //
+// ____________________ Sticky navigation ____________________ //
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
